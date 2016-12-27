b629457bbbb29e965b786c131e840dee1ea589b7
Thilladon <Thilladon@gmail.com>
2016-12-27 18:28:05 +0100

    Ajout de include.js + de l'ennemi

--- js/game.js
+++ js/game.js
@@ -1,3 +1,11 @@
+// Inclusion des class.
+
+include([
+	'../js/class/character.js',
+	'../js/class/map.js',
+	'../js/class/level.js'
+]);
+
 var canvas = null;
 var background = null;
 var ctx = null;
@@ -32,16 +40,21 @@ window.onload = function()
 
 	var sources = {
 		map 		: '../img/game/map_sprite.png',
-		thilladon	: '../img/game/character.png',
+		user		: '../img/game/character.png',
+		fly			: '../img/game/fly.png',
 		background	: '../img/game/background.png',
 		foreground	: '../img/game/foreground.png'
 	};
 
 	loadImages(sources, function(images)
 	{
-		thilladon = new player(images.thilladon.src, "Thilladon", 1, 8, 8, DIRECTION.BAS);
+		fly = new player(images.fly.src, "fly", 2, 7, 7, DIRECTION.BAS);
+		fly2 = new player(images.fly.src, "fly", 2, 9, 9, DIRECTION.BAS);
+		player = new player(images.user.src, "Thilladon", 1, 8, 8, DIRECTION.BAS);
 		level = new map(ctx, images.map.src, images.background.src, images.foreground.src, level[0], worldWidth, worldHeight, 32, 32);
-		level.addCharacter(thilladon);
+		level.addCharacter(fly);
+		level.addCharacter(fly2);
+		level.addCharacter(player);
 		level.image.onload = function() { 
 			Interval = setInterval(function() {
 				level.draw();
@@ -57,16 +70,16 @@ window.onload = function()
 
 		switch(key) {
 			case 38 : case 122 : case 119 : case 90 : case 87 : // Flèche haut, z, w, Z, W
-				thilladon.deplacer(DIRECTION.HAUT, level);
+				player.deplacer(DIRECTION.HAUT, level);
 				break;
 			case 40 : case 115 : case 83 : // Flèche bas, s, S
-				thilladon.deplacer(DIRECTION.BAS, level);
+				player.deplacer(DIRECTION.BAS, level);
 				break;
 			case 37 : case 113 : case 97 : case 81 : case 65 : // Flèche gauche, q, a, Q, A
-				thilladon.deplacer(DIRECTION.GAUCHE, level);
+				player.deplacer(DIRECTION.GAUCHE, level);
 				break;
 			case 39 : case 100 : case 68 : // Flèche droite, d, D
-				thilladon.deplacer(DIRECTION.DROITE, level);
+				player.deplacer(DIRECTION.DROITE, level);
 				break;
 			default : 
 				//alert(key);
